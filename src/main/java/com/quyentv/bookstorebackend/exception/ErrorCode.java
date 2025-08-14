443c5cb1e28a106b7f097dd7da8eca7395e9aedc
quyen-tv <tvq05112003@gmail.com>
2025-08-14 19:02:06 +0700

    :recycle: refactor(exception): improve global exception handling
    - A new AppException class to standardize custom exceptions.
    - An ErrorCode enum for better error message management.
    - An updated GlobalExceptionHandler to handle these new error types.

---
+++ src/main/java/com/quyentv/bookstorebackend/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.quyentv.bookstorebackend.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+    private final int code;
+    private final String message;
+    private final HttpStatusCode statusCode;
+}
---
+++ src/main/java/com/quyentv/bookstorebackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.quyentv.bookstorebackend.exception;
+
+import com.quyentv.bookstorebackend.dto.response.ApiResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+     ResponseEntity<ApiResponse<?>> handlingRuntimeException(RuntimeException exception) {
+        log.error(exception.getMessage(), exception);
+        ApiResponse<?> apiResponse = new ApiResponse<>();
+
+        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse<?>> handlingAppException(AppException exception) {
+        log.error(exception.getMessage(), exception);
+        ErrorCode errorCode = exception.getErrorCode();
+        ApiResponse<?> apiResponse = new ApiResponse<>();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
+    }
+}
