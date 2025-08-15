1301648ca6efd0d084b9b00f80a5c0bfb942f2cc
quyen-tv <tvq05112003@gmail.com>
2025-08-15 10:30:52 +0700

    :sparkles: feat(auth): add authentication service, DTOs and implement authentication API endpoint

---
+++ src/main/java/com/quyentv/bookstorebackend/dto/response/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.quyentv.bookstorebackend.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthenticationResponse {
+
+    String token;
+    boolean authenticated;
+}
