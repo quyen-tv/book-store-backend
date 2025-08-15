51a16e4f63d19ee23c1137bd72d6adb4cf867c19
quyen-tv <tvq05112003@gmail.com>
2025-08-15 09:30:31 +0700

    :sparkles: feat(user): create UserMapper for DTO and entity conversion

---
+++ book-store-backend/src/main/java/com/quyentv/bookstorebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.quyentv.bookstorebackend.mapper;
+
+import com.quyentv.bookstorebackend.dto.request.UserCreationRequest;
+import com.quyentv.bookstorebackend.dto.request.UserUpdateRequest;
+import com.quyentv.bookstorebackend.dto.response.UserResponse;
+import com.quyentv.bookstorebackend.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toUser(UserCreationRequest request);
+
+    UserResponse toUserResponse(User user);
+
+    @Mapping(target = "roles", ignore = true)
+    void updateUser(@MappingTarget User user, UserUpdateRequest request);
+}
