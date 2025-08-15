46551c265250d6414e38b2776fa32c6e6450ee43
quyen-tv <tvq05112003@gmail.com>
2025-08-15 09:05:30 +0700

    :bug: fix(repository): correctly annotate repository interfaces with @Repository

--- src/main/java/com/quyentv/bookstorebackend/repository/UserRepository.java
+++ src/main/java/com/quyentv/bookstorebackend/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.quyentv.bookstorebackend.repository;
 
 import com.quyentv.bookstorebackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByUsername(String username);
 
