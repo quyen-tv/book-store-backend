f6b013b2b4fe0c12d8a608142abb54c5faae662a
quyen-tv <tvq05112003@gmail.com>
2025-08-15 09:45:51 +0700

    :recycle: refactor(exception): add validation error handler and specific error codes

--- src/main/java/com/quyentv/bookstorebackend/exception/ErrorCode.java
+++ src/main/java/com/quyentv/bookstorebackend/exception/ErrorCode.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR);
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_EXISTED(409, "User existed", HttpStatus.BAD_REQUEST),
+    INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(400, "Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(400, "Password must be at least {min} characters", HttpStatus.BAD_REQUEST);
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
