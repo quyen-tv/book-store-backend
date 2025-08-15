a7455866ef3c444df6455cb96da4951966f3acc0
quyen-tv <tvq05112003@gmail.com>
2025-08-15 09:45:51 +0700

    :recycle: refactor(exception): add validation error handler and specific error codes

--- book-store-backend/src/main/java/com/quyentv/bookstorebackend/exception/GlobalExceptionHandler.java
+++ book-store-backend/src/main/java/com/quyentv/bookstorebackend/exception/GlobalExceptionHandler.java
@@ -2,15 +2,23 @@ package com.quyentv.bookstorebackend.exception;
 
 import com.quyentv.bookstorebackend.dto.response.ApiResponse;
 
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
+
     @ExceptionHandler(value = Exception.class)
      ResponseEntity<ApiResponse<?>> handlingRuntimeException(RuntimeException exception) {
         log.error(exception.getMessage(), exception);
@@ -32,5 +40,42 @@ public class GlobalExceptionHandler {
         apiResponse.setMessage(errorCode.getMessage());
 
         return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<ApiResponse<?>> handlingValidation(MethodArgumentNotValidException exception) {
+        String enumKey = exception.getFieldError().getDefaultMessage();
+
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map <String, Object> attributes = null;
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation =
+                    exception.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
+            log.info(attributes.toString());
+
+        } catch (IllegalArgumentException e) {
+            log.error(e.getMessage(), e);
+        }
+
+        ApiResponse<?> apiResponse = new ApiResponse<>();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(
+                Objects.nonNull(attributes)
+                        ? mapAttribute(errorCode.getMessage(), attributes)
+                        : errorCode.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 }
