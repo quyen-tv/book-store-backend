d386cb0d8ec2a844d129924129e62fa8a6a2412c
quyen-tv <tvq05112003@gmail.com>
2025-08-15 09:05:30 +0700

    :bug: fix(repository): correctly annotate repository interfaces with @Repository

--- book-store-backend/src/main/java/com/quyentv/bookstorebackend/repository/PermissionRepository.java
+++ book-store-backend/src/main/java/com/quyentv/bookstorebackend/repository/PermissionRepository.java
@@ -1,2 +1,9 @@
-package com.quyentv.bookstorebackend.repository;public interface PermissionRepository {
+package com.quyentv.bookstorebackend.repository;
+
+import com.quyentv.bookstorebackend.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, String> {
 }
--- book-store-backend/src/main/java/com/quyentv/bookstorebackend/repository/RoleRepository.java
+++ book-store-backend/src/main/java/com/quyentv/bookstorebackend/repository/RoleRepository.java
@@ -1,2 +1,11 @@
-package com.quyentv.bookstorebackend.repository;public interface RoleRepository {
+package com.quyentv.bookstorebackend.repository;
+
+import com.quyentv.bookstorebackend.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+
+
 }
